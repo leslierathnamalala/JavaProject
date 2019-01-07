35e2fe74e3b77dac0b87189e3f96855f32e8f38d
anjaleeimalka96 <anjaleeimalka96@outlook.com>
2019-01-07 17:20:54 +0530

    sample
    select

--- src/javaproject/Panels/VacantRoom.java
+++ src/javaproject/Panels/VacantRoom.java
@@ -38,6 +38,11 @@ public class VacantRoom extends javax.swing.JPanel {
         jTextField1.setText("jTextField1");
 
         jButton1.setText("Vacant");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -65,6 +70,10 @@ public class VacantRoom extends javax.swing.JPanel {
                 .addContainerGap(310, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
