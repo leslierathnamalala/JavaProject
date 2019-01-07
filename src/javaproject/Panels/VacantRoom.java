81e21e952de51f6c2d1ea71f581e65b038642e5b
gayashie <45633608+gayashie@users.noreply.github.com>
2019-01-07 17:29:45 +0530

    comment
    comment

--- src/javaproject/Panels/VacantRoom.java
+++ src/javaproject/Panels/VacantRoom.java
@@ -38,6 +38,11 @@ public class VacantRoom extends javax.swing.JPanel {
         jTextField1.setText("jTextField1");
 
         jButton1.setText("Vacant");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -66,7 +71,11 @@ public class VacantRoom extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
+//
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
