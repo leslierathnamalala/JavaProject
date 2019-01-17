0eeb5b92bd77b22257682c8ae3e5646b7df5399b
gayashie <45633608+gayashie@users.noreply.github.com>
2019-01-17 11:01:01 +0530

    login
    login form

--- src/javaproject/Panels/VacantRoom.java
+++ src/javaproject/Panels/VacantRoom.java
@@ -10,7 +10,7 @@ package javaproject.Panels;
  * @author Leslie_Rathnamalala
  */
 public class VacantRoom extends javax.swing.JPanel {
-//
+
     /**
      * Creates new form VacantRoom
      */
@@ -28,14 +28,19 @@ public class VacantRoom extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        fullname = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(255, 204, 153));
 
         jLabel1.setText("jLabel1");
 
-        jTextField1.setText("jTextField1");
+        fullname.setText("jTextField1");
+        fullname.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                fullnameActionPerformed(evt);
+            }
+        });
 
         jButton1.setText("Vacant");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -55,7 +60,7 @@ public class VacantRoom extends javax.swing.JPanel {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addGap(26, 26, 26)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 349, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(fullname, javax.swing.GroupLayout.PREFERRED_SIZE, 349, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(167, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -64,7 +69,7 @@ public class VacantRoom extends javax.swing.JPanel {
                 .addGap(68, 68, 68)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(fullname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(59, 59, 59)
                 .addComponent(jButton1)
                 .addContainerGap(310, Short.MAX_VALUE))
@@ -73,12 +78,18 @@ public class VacantRoom extends javax.swing.JPanel {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        
+       
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void fullnameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fullnameActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_fullnameActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField fullname;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
